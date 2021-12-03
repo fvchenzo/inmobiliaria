ce850cd5d198d782f34da9400475ab4a7ccc8d72
Fernando Vinchenzo Aragón Palomino <58162684+fvchenzo@users.noreply.github.com>
2021-12-02 21:11:27 -0500

    sitio web dinamico
    se agregó servicios y las modificaciones en los componentes project, apartment para hacer que el sitio sea dinamico

--- src/app/pages/apartment/apartment.component.ts
+++ src/app/pages/apartment/apartment.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { ApartmentService } from 'src/app/services/apartment.service';
+import { ProjectService } from 'src/app/services/project.service';
 
 @Component({
   selector: 'app-apartment',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApartmentComponent implements OnInit {
 
-  constructor() { }
+  project: any = []
+  apartment: any = []
+
+  constructor(
+    private readonly ps: ProjectService,
+    private readonly as: ApartmentService,
+    private ar: ActivatedRoute
+  ) { }
+
+  __getProjectById(id: number) {
+    this.ps.__getProjects().subscribe((rest: any) => {
+      this.project = rest.data.filter((item: { id: number}) => item.id == id)
+      //console.log(this.project)
+    })
+  }
+
+  __getApartmentById(id: number) {
+    this.as.__getApartments().subscribe((rest: any) => {
+      this.apartment = rest.data.filter((item: { id: number}) => item.id == id)
+      //console.log(this.apartment)
+    })
+  }
 
   ngOnInit(): void {
+    this.ar.params.subscribe((params: Params) => {
+      if (params.id && params.projectId) {
+        this.__getProjectById(params.projectId)
+        this.__getApartmentById(params.id)
+      }
+    })
   }
 
 }
